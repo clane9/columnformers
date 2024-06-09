86254a5b4fe6f3dcf11ac58930d0b5885f835eae
Connor Lane <connor.lane858@gmail.com>
2024-06-08 20:52:59 -0400

    Refactor wiring cost

--- topomoe/models/wiring.py
+++ topomoe/models/wiring.py
@@ -1,13 +1,30 @@
+from typing import Optional
+
 import torch
 import torch.nn.functional as F
 from torch import nn
 
 
 class L1WiringCost(nn.Module):
-    def __init__(self, dist: torch.Tensor, lambd: float = 0.01, p: float = 1.0):
+    """
+    Distance weighted L1 wiring cost. Distance can be raised to a power to penalize
+    long-range connections more strongly.
+    """
+
+    def __init__(
+        self,
+        geo_embed: torch.Tensor,
+        in_geo_embed: Optional[torch.Tensor] = None,
+        lambd: float = 0.01,
+        p: float = 1.0,
+    ):
         super().__init__()
         self.lambd = lambd
         self.p = p
+
+        if in_geo_embed is None:
+            in_geo_embed = geo_embed
+        dist = torch.cdist(geo_embed, in_geo_embed)
 
         weight = dist**p
         self.register_buffer("weight", weight)
@@ -21,10 +38,28 @@ class L1WiringCost(nn.Module):
 
 
 class CrossEntropyWiringCost(nn.Module):
-    def __init__(self, dist: torch.Tensor, lambd: float = 0.01, sigma: float = 2.0):
+    """
+    Cross-entropy wiring cost, penalizing `- (A * W.log()).sum()` where `W` is based on
+    gaussian weighted probabilities. `W = softmax(0.5 * D ** 2 / sigma ** 2)`.
+
+    Similar to the distance-weighted L1 wiring cost with `p=2`, but does not decay all
+    the way to 0 for distance 0 or penalize long connections quite as strongly.
+    """
+
+    def __init__(
+        self,
+        geo_embed: torch.Tensor,
+        in_geo_embed: Optional[torch.Tensor] = None,
+        lambd: float = 0.01,
+        sigma: float = 2.0,
+    ):
         super().__init__()
         self.lambd = lambd
         self.sigma = sigma
+
+        if in_geo_embed is None:
+            in_geo_embed = geo_embed
+        dist = torch.cdist(geo_embed, in_geo_embed)
 
         weight = -0.5 * dist**2 / sigma**2
         weight = -F.log_softmax(weight, dim=-1)
