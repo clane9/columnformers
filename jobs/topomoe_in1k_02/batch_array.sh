f1a29928ffef5cedb6e89f35b6019e5c185ad82c
Connor Lane <connor.lane858@gmail.com>
2024-12-11 19:00:44 -0500

    ImageNet configs (#22)
    * Update imagenet1k training configs
    
    * Add second imagenet training job
    
    * Update imagenet configs batch size, lr
    
    * Add more imagenet configs

--- jobs/topomoe_in1k_02/batch_array.sh
+++ jobs/topomoe_in1k_02/batch_array.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#SBATCH --job-name=topomoe_in1k_01
+#SBATCH --job-name=topomoe_in1k_02
 #SBATCH --partition=GPU-shared
 #SBATCH -N 1
 #SBATCH --ntasks=10
-#SBATCH --gpus=v100-32:2
+#SBATCH --gpus=v100-32:1
 #SBATCH --time=1-00:00:00
-#SBATCH --array=1
+#SBATCH --array=0
 #SBATCH --account=med220004p
 
 export OMP_NUM_THREADS=10
@@ -22,18 +22,20 @@ source .venv/bin/activate
 source .env
 wandb login
 
-JOB="topomoe_in1k_01"
+JOB="topomoe_in1k_02"
 # ls | xargs -I {} basename {} .yaml
 NAMES=(
-    01_topomoe_small_3s
-    02_vit_small
+    01_vit_small
+    02_topomoe_small_3s_wl-0.01
+    03_topomoe_small_3s_wl-0.001
+    04_topomoe_small_3s_dyn_wl-0.001
 )
 
 NAME="${NAMES[SLURM_ARRAY_TASK_ID]}"
 CONFIG="jobs/${JOB}/configs/${NAME}.yaml"
 FULL_NAME="${JOB}/${NAME}"
 
-torchrun --standalone --nproc_per_node=2 \
+torchrun --standalone --nproc_per_node=1 \
     src/topomoe/train.py \
     $CONFIG \
     --name $FULL_NAME
